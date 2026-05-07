53263fec4de425d011e51affdea06be673166940
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-07 22:17:45 +0900

    fix:learn keyword & deafult arguments

--- functions.py
+++ functions.py
@@ -5,12 +5,12 @@
 (4) Scope 
 '''
 
-print("====DEFINE vs CALL =====")
+print("====DEFINE(parametr) vs CALL(argument) =====")
 # build in function > print() type ()
 # Function -reusable block of code!
 # Instead of block {} in JAvA, Python uses indentation!
 
-# DEFINE - build
+# DEFINE - build - parametr
 
 
 def greet(a):
@@ -22,9 +22,26 @@ def greeting(b):
     return f"hi {b}"
 
 
-# Call -execute
+# Call -execute - argument
 result1 = greet('Sanchez')
 print("result1:", result1)
 
 result2 = greeting("Sam")
 print("result2:", result2)
+
+
+print("====Keyword & default arguments =====")
+# DEFINE
+
+
+def give_greet(name, age=22):
+    print("give_great is executed")
+    return f"hi {name}, you are {age} years old!"
+
+
+# CALL
+result3 = give_greet(name="Justin", age=28)
+print("result3:", result3)
+
+result4 = give_greet("john")
+print("result4:", result4)
