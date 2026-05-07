b6ee9e7bc6862634c125177cce03a5add339fa7f
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-07 19:51:44 +0900

    feat: learn string in Python

--- primitive.py
+++ primitive.py
@@ -12,3 +12,21 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+print("======string=====")
+# METHODS: upper() loewr() title() find() replace()
+
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "Masterclass")
+print(f"the result (4): {result}")
+
+print(course)
