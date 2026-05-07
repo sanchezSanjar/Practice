89f1e886c5423f126fa84b29e9ff856f80d6a887
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-07 20:25:58 +0900

    feat:learn python boolean

--- primitive.py
+++ primitive.py
@@ -29,4 +29,21 @@ print(f"the result (3): {result}")
 result = course.replace("Fullstack", "Masterclass")
 print(f"the result (4): {result}")
 
-print(course)
+print("======boolean=====")
+# functions > type() input() bool() int() str()
+y = input("Givr your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+
+# TRUTHY vs FALSY value
+# TRUTHY: True 100 -100 "MIT"
+# FALSY: False 0 "" None
+
+test_falsy = "" or False or None or 0
+print("test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
