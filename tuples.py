41acbf4d85579c6ff5cbf937b5efd72c00962b74
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-14 22:46:04 +0900

    feat:modify usage of *args/tuple and **kwargs/dict together and  learn zip object

---
+++ tuples.py
@@ -0,0 +1,89 @@
+'''Tuple
+    (1) What is tuple: typle vs list
+    (2) Unpacking arguments
+    (3) zip
+'''
+
+print("==== what is tuple: typle vs list ====")
+# Java/PHP/NodeJS array => Python list
+
+# literal
+numbs = [3, 5, 1, 2]
+
+# constructor
+letters = list("Hello World!")
+
+fruits = ["apple", "lemon", "banana", "kiwi"]
+print("before fruits:", fruits)
+
+fruits[2] = "melon"
+print("after fruits:", fruits)
+
+# we can not mutate tuple
+animals = ("dog", "cat", "fish", "lion")
+tuple_obj = ("MIT", 100, True, None)
+
+print(animals[0])
+# animals[0] = "bird"
+
+# try avoid this
+people = "Andrew", "John"
+animals = "dog",
+
+print("===== Unpacking arguments")
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+(x, y, z, a) = groups
+print(f"the x: {x} and y {y}")
+print("z:", z)  # list
+
+
+# *args >tuple
+def calculate(*args):
+    print("*args >", args)
+    total = 1
+    for x in args:
+        total *= x
+    print(f"the type(args) value: {type(args)}")
+    print(f"the total value: {total}")
+    return total
+
+
+# CALL
+calculate(1, 7, 2, 3)
+print("-----")
+calculate(0, 2, 300)
+print("-----")
+calculate(5, 7)
+
+
+print("-----")
+# **kwargs > dictionary
+
+
+def introduce(**kwargs):
+    print(f"the type(**kwargs) value: {type(kwargs)}")
+    print(f"Hi, I am {kwargs["name"]} and I am {kwargs["age"]} years old!")
+
+
+# CALL
+introduce(name="Justin", age=28)
+introduce(name="Shawn", age=30, single=True)
+print("-------")
+
+
+def greeting(*args, **kwargs):
+    print("*args >", args)
+    print("**kwargs >", kwargs)
+
+
+# CALL
+greeting("hi", True, 10, name="John", age=22)
+
+print("==== zip =====")
+tuple1 = (1, 2, 3, 4)
+tuple2 = ('a', 'b', 'c')
+
+zipped = zip(tuple1, tuple2)
+print("zipped:", zipped)
+result = list(zipped)
+print(f"the result: {result}")
