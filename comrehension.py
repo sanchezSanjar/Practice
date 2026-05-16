9ff0e24678eac49722a2ea5d69b16b2027f799d2
SanjarSanchez <sanjar@ajou.ac.kr>
2026-05-17 08:02:20 +0900

    feat:set dictionary comprehension

--- comrehension.py
+++ comrehension.py
@@ -34,3 +34,18 @@ cars = [
 ]
 list_cars = [car[0] for car in cars if car[1] > 80]  # c version
 print("list_cars:", list_cars)
+
+
+print("===== set and dictionary comperehension =====")
+numbs = [1, 5, 4, 20, 4, 5, 1, 4]
+set_numbs = {*numbs}
+print("set_numbs:", set_numbs)
+
+dict_people = {person[0]: person[1] for person in people}  # b version
+print("dict_people:", dict_people)
+
+dict_people2 = {person[0]: person[1]
+                for person in people if person[1] > 20}  # c version
+print("dict_people2:", dict_people2)
+
+# (<expression> for item in iterable) generic
